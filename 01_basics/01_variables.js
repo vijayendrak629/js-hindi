9ad31ceaf155f4d928f08420466fd4043b22c1d4
Vijayendra Kumbhar <87369447+vijayendrak629@users.noreply.github.com>
2023-10-19 17:18:32 +0000

    Data Types

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,28 @@
+const accountId = 144553;
+let accountEmail = "vijay@google.com";
+var accoountPassword = "12345";
+accountCity = "Jaipur";
+
+//accountId = 2
+accountEmail = "vk@google.com";
+accoountPassword = 216545;
+//Data types
+// number => 2 to power 53
+// bigint  
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+
+
+// console.log(accountId);
+
+// console.table([accountId, accountEmail, accoountPassword, accountCity]);
+
+console.log(typeof accoountPassword);
+
+//object
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,7 @@
+"use strict"; //treat all JS code as a new version
+
+// alert( 3 + 3)  // we r using nodejs, not browser
+
+console.log(3 + 3);
+
+console.log("Hitesh");
