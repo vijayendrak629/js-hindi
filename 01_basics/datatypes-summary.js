929f24196a83b62c76458d497c02899cc384eaf4
Vijayendra Kumbhar <87369447+vijayendrak629@users.noreply.github.com>
2023-10-19 20:21:07 +0000

    updated datatypes summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -11,12 +11,11 @@
 // const isLoggedIn = false;
 // console.log(typeof isLoggedIn);
 
-const outsideTemp = null;
-console.log(typeof outsideTemp);
+// const outsideTemp = null;
+// console.log(typeof outsideTemp);
 
 // let userEmail;
 // console.log(typeof userEmail);
-
 
 // const id = Symbol("1234");
 // const anotherId = Symbol("1234");
@@ -24,27 +23,49 @@ console.log(typeof outsideTemp);
 // console.log(id === anotherId);
 // console.log(typeof id);
 
-const bigNumber = 212121212n
+// const bigNumber = 212121212n
 
-console.log(typeof bigNumber);
+// console.log(typeof bigNumber);
 
 // Non-primitive data type
 
-const heroes = ["Shaktiman","Soldier","doga"];
+// const heroes = ["Shaktiman","Soldier","doga"];
 
-let myObj = {
-    name: "Vijay",
-    age: 22,
-}
+// let myObj = {
+//     name: "Vijay",
+//     age: 22,
+// }
 
-const myFunction = function(){
-console.log("Hello World");
-}
+// const myFunction = function(){
+// console.log("Hello World");
+// }
 
-console.log(typeof myFunction);
-console.log(typeof heroes);
-console.log(typeof myObj);
+// console.log(typeof myFunction);
+// console.log(typeof heroes);
+// console.log(typeof myObj);
 
+//  *****************************************************
 
+// Stack(Primitive), Heap (Non-primitive)
 
+let myYoutubename = "Chai aur code";
 
+let myanothername = "coffe aur code";
+
+myanothername = "only code";
+console.log(myYoutubename);
+console.log(myanothername);
+
+// Heap
+
+let userOne = {
+  name: "Vijay",
+  email: "vijay@gmail.com",
+};
+
+userTwo = userOne
+
+userTwo.email=  "Vijay321@gmail.com";
+
+console.log(userOne.email);
+console.log(userTwo.email);
