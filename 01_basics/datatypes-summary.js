5df50a0d2c899d3816d1a21a81c6b64239dbe0a5
Vijayendra Kumbhar <87369447+vijayendrak629@users.noreply.github.com>
2023-10-19 19:54:47 +0000

    data type summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,50 @@
+// Primitive data types
+
+// Data types : String, Number, Boolean, null, undefined, symbol, bigint
+
+// const score = "100";
+// console.log(typeof score);
+
+// const scoreValue = 100.3;
+// console.log(typeof scoreValue);
+
+// const isLoggedIn = false;
+// console.log(typeof isLoggedIn);
+
+const outsideTemp = null;
+console.log(typeof outsideTemp);
+
+// let userEmail;
+// console.log(typeof userEmail);
+
+
+// const id = Symbol("1234");
+// const anotherId = Symbol("1234");
+
+// console.log(id === anotherId);
+// console.log(typeof id);
+
+const bigNumber = 212121212n
+
+console.log(typeof bigNumber);
+
+// Non-primitive data type
+
+const heroes = ["Shaktiman","Soldier","doga"];
+
+let myObj = {
+    name: "Vijay",
+    age: 22,
+}
+
+const myFunction = function(){
+console.log("Hello World");
+}
+
+console.log(typeof myFunction);
+console.log(typeof heroes);
+console.log(typeof myObj);
+
+
+
+
