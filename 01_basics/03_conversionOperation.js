556aaf4f846a7e432f374b831497d81134b562f1
Vijayendra Kumbhar <87369447+vijayendrak629@users.noreply.github.com>
2023-10-19 17:37:37 +0000

    conversion operation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,27 @@
+let score = "33abc";
+
+
+// console.log(score);
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+
+// let isNumber = Number(score);
+// console.log(isNumber);
+// console.log(typeof isNumber);
+
+// let isLoggedIn = "Vijay"
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+let someNumber = 22
+
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
+
+
