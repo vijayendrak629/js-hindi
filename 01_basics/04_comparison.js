02229f9ebaaff9ed6bea3ef72b63f3b825799c0b
Vijayendra Kumbhar <87369447+vijayendrak629@users.noreply.github.com>
2023-10-19 18:59:38 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(5 > 3);
+// console.log(5 < 3);
+// console.log(3 >= 3);
+// console.log(5 == 3);
+// console.log(5 != 3);
+
+// console.log("2" > 3);
+// console.log("02" > 3);
+
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined == 0);
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+
+console.log(2 === 2);
