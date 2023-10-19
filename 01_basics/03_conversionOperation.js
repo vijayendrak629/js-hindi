8d5a3bc9b71d3d1c8a87b90a7dc4bb7f38604e66
Vijayendra Kumbhar <87369447+vijayendrak629@users.noreply.github.com>
2023-10-19 17:56:19 +0000

    conversion of data types

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -23,5 +23,16 @@ let stringNumber = String(someNumber);
 console.log(stringNumber);
 console.log(typeof stringNumber);
 
+//"33" => 33
+// "33abc" => NaN
+// true => 1; false => 0;
+
+// boolean
+// 1 => true; 0 => false
+// "" => false
+// "Vijay" => true
+
+
+
 
 
