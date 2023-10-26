e7d682f384bdeb65e68d1e382403c87a315ed562
Vijayendra Kumbhar <87369447+vijayendrak629@users.noreply.github.com>
2023-10-26 20:17:50 +0000

    scope

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,28 @@
+let a = 30;
+const b = 30;
+var c = 40;
+
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+if(true){
+    let a = 20;
+}
+
+// console.log(a);
+
+
+
+if(true){
+    
+    function myFunction() {
+    const name="Vijay"  
+    console.log(name); 
+    }
+    myFunction();
+
+}
+
+
+
